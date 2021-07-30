8e582478db5c2468de3b15d185e2f6b9543330b2
vincentzyc <vincentzyc@qq.com>
2021-07-30 10:57:29 +0800

    🎉init: vuepress

---
+++ docs/.vuepress/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="home-title">Hello Nuage</h1>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+.home-title {
+  text-align: center;
+}
+</style>
